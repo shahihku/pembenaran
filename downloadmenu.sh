b18886c1997c1217fc05f5438daf743e09990d1c
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-20 00:53:30 +0700

    Create downloadmenu.sh

---
+++ downloadmenu.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ==========================================
+# Color
+# download menu
+cd /root
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-on.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addtj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/backup.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cek-service.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cektj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cekvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cekvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cert.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/clear-log.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/clear-ram.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/delexp.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/deltj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/delvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/delvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewtj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/restart.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/speedtest.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/xp.sh
+chmod +x /root/add-on.sh
+chmod +x /root/addtj.sh
+chmod +x /root/addvl.sh
+chmod +x /root/addvm.sh
+chmod +x /root/backup.sh
+chmod +x /root/cek-service.sh
+chmod +x /root/cektj.sh
+chmod +x /root/cekvl.sh
+chmod +x /root/cekvm.sh
+chmod +x /root/cert.sh
+chmod +x /root/clear-log.sh
+chmod +x /root/clear-ram.sh
+chmod +x /root/delexp.sh
+chmod +x /root/deltj.sh
+chmod +x /root/delvl.sh
+chmod +x /root/delvm.sh
+chmod +x /root/renewtj.sh
+chmod +x /root/renewvl.sh
+chmod +x /root/renewvm.sh
+chmod +x /root/restart.sh
+chmod +x /root/speedtest.sh
+chmod +x /root/xp.sh
+cd
