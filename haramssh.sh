bc3e44cd98d3e5b017125e0d9aad4a244648268f
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-20 03:21:55 +0700

    Update haramssh.sh

--- haramssh.sh
+++ haramssh.sh
@@ -2,16 +2,15 @@
 # ==========================================
 # Color
 # download menu
-cd /usr/bin
-wget -O addssh "https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/addssh.sh"
-wget -O cekssh "https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/cekssh.sh"
-wget -O delssh "https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/delssh.sh"
-wget -O renewssh "https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/renewssh.sh"
-chmod +x /usr/bin/addssh
-chmod +x /usr/bin/cekssh
-chmod +x /usr/bin/delssh
-chmod +x /usr/bin/renewssh
 cd /root
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/addssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/cekssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/delssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/renewssh.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/sshsaja/install2.sh
+chmod +x /root/addssh
+chmod +x /root/cekssh
+chmod +x /root/delssh
+chmod +x /root/renewssh
 chmod +x /root/install2.sh
 cd
