2471e5c2d2c8797fd5795cd977fa8116e5cc9426
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-25 03:01:17 +0700

    Create menutj.sh

---
+++ menu/menutj.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(cat /etc/xray/public)
+clear
+echo -e "\e[32m════════════════════════════════════════" | lolcat
+echo -e "             ═══[Trojan MahaVPN]═══"
+echo -e "\e[32m════════════════════════════════════════" | lolcat
+echo -e " 1)  Create Trojan S"
+echo -e " 2)  Create Trojan M"
+echo -e " 3)  Deleting Trojan Account"
+echo -e " 4)  Renew Trojan Account"
+echo -e " 5)  Check User Login Trojan"
+echo -e ""
+echo -e "\e[1;32m══════════════════════════════════════════\e[m"
+echo -e " x)   MENU UTAMA"
+echo -e "\e[1;32m══════════════════════════════════════════\e[m"
+echo -e ""
+read -p "     Please Input Number  [1-4 or x] :  "  menu
+echo -e ""
+case $menu in
+1)
+add-trojan
+;;
+2)
+addtj
+;;
+3)
+deltj
+;;
+4)
+renewtj
+;;
+5)
+cektj
+;;
+x)
+gboy
+;;
+*)
+echo "Please enter an correct number"
+sleep 1
+menutj
+;;
+esac
+
