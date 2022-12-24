8eb0aa7bc6205c2fb3864e247433a9b549b91e6d
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-25 03:37:39 +0700

    Update haramnossh.sh

--- haramnossh.sh
+++ haramnossh.sh
@@ -3,10 +3,22 @@
 # Color
 # download menu
 cd /root
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-domain.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-on.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-trojan.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-vless.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/add-vmess.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addtj.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addvl.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/addvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewtj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/gboy.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/menutj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/menuvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/menuvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/menusys.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/backup.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cek-service.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/cektj.sh
@@ -20,17 +32,29 @@ wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/deltj.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/delvl.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/delvm.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/install.sh
-wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewtj.sh
-wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvl.sh
-wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/renewvm.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/restart.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/speedtest.sh
 wget https://raw.githubusercontent.com/shahihku/pembenaran/main/menu/xp.sh
 wget https://raw.githubusercontent.com/ProcopasTV/nyoabain/main/skc.sh
+chmod +x /root/add-domain.sh
+chmod +x /root/add-trojan.sh
+chmod +x /root/add-vless.sh
+chmod +x /root/add-vmess.sh
+chmod +x /root/renewtj.sh
+chmod +x /root/renewvl.sh
+chmod +x /root/renewvm.sh
+chmod +x /root/gboy.sh
+chmod +x /root/menutj.sh
+chmod +x /root/menuvl.sh
+chmod +x /root/menuvm.sh
+chmod +x /root/menusys.sh
 chmod +x /root/add-on.sh
 chmod +x /root/addtj.sh
 chmod +x /root/addvl.sh
 chmod +x /root/addvm.sh
+chmod +x /root/renewtj.sh
+chmod +x /root/renewvl.sh
+chmod +x /root/renewvm.sh
 chmod +x /root/backup.sh
 chmod +x /root/cek-service.sh
 chmod +x /root/cektj.sh
@@ -44,9 +68,6 @@ chmod +x /root/deltj.sh
 chmod +x /root/delvl.sh
 chmod +x /root/delvm.sh
 chmod +x /root/install.sh
-chmod +x /root/renewtj.sh
-chmod +x /root/renewvl.sh
-chmod +x /root/renewvm.sh
 chmod +x /root/restart.sh
 chmod +x /root/speedtest.sh
 chmod +x /root/xp.sh
